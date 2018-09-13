e90ecb34bf8c4d6faf6e466804cf2f6c9aa30387
zxsn <1004700604@qq.com>
2018-09-13 17:58:51 +0800

    2018年9月13日17点58分

---
+++ src/com/java/development/five/string/StringDemo01.java
@@ -0,0 +1,34 @@
+/** 
+ * projectName:Java开发实战经典 
+ * fileName:StringDemo01.java 
+ * packageName:com.java.development.five.string 
+ * date:2018年9月13日下午5:49:05 
+ * copyright(c) 2017-2020 xxx公司
+ */
+package com.java.development.five.string;
+
+/**   
+ * @title: StringDemo01.java 
+ * @package com.java.development.five.string 
+ * @description: TODO
+ * @author: zxsn
+ * @date: 2018年9月13日 下午5:49:05 
+ * @version: V1.0   
+*/
+public class StringDemo01 {
+
+    /**
+     *@title main 
+     *@description: TODO
+     *@author: zxsn
+     *@date: 2018年9月13日 下午5:49:05
+     *@param args
+     *@throws 
+     */
+    public static void main(String[] args) {
+        String name = "JiaLiXin"; //实例化String对象
+        System.out.println("姓名：" + name); //输出字符串的内容
+
+    }
+
+}
---
+++ src/com/java/development/five/string/StringDemo02.java
@@ -0,0 +1,34 @@
+/** 
+ * projectName:Java开发实战经典 
+ * fileName:StringDemo02.java 
+ * packageName:com.java.development.five.string 
+ * date:2018年9月13日下午5:54:26 
+ * copyright(c) 2017-2020 xxx公司
+ */
+package com.java.development.five.string;
+
+/**   
+ * @title: StringDemo02.java 
+ * @package com.java.development.five.string 
+ * @description: TODO
+ * @author: zxsn
+ * @date: 2018年9月13日 下午5:54:26 
+ * @version: V1.0   
+*/
+public class StringDemo02 {
+
+    /**
+     *@title main 
+     *@description: TODO
+     *@author: zxsn
+     *@date: 2018年9月13日 下午5:54:26
+     *@param args
+     *@throws 
+     */
+    public static void main(String[] args) {
+        String name = new String("JiaLiXin");
+        System.out.println("姓名：" + name);
+
+    }
+
+}
